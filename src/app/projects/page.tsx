4087b3f1445bd83ca07918ba1b1a6fbbb04543fe
Jahsfreedman Moffat <xcoder2442@gmail.com>
2025-11-12 12:48:14 +0000

    get this ready for deployment

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -36,7 +36,7 @@ export default function ProjectsPage({
         <CategoryFilters categories={categories} />
       </div>
       
-      <div className="grid grid-cols-3 gap-4 md:gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
         {filteredProjects.map((project, index) => (
           <div
             key={project.slug}
